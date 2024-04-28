21ae2c9923cea3daebe542c9e5b2239567ef1fe1
coderaiser <coderaiser@cloudcmd.io>
2024-04-28 19:57:58 +0300

    feature: bin: add

--- bin/quick-lint.mjs
+++ bin/quick-lint.mjs
@@ -2,6 +2,12 @@ import {readFile} from 'fs/promises';
 import {lint} from '../lib/quick-lint.js';
 
 const fileName = process.argv[2];
+
+if (!fileName) {
+    console.log('quick-lint [file name]');
+    process.exit(0);
+}
+
 const source = await readFile(fileName, "utf-8");
 const places = await lint(source);
 
