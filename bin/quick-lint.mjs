925a4947f11702770339a94e8ead8f0f19325ba9
coderaiser <coderaiser@cloudcmd.io>
2024-04-28 19:59:25 +0300

    chore: lint

--- bin/quick-lint.mjs
+++ bin/quick-lint.mjs
@@ -1,4 +1,6 @@
-import {readFile} from 'fs/promises';
+#!/usr/bin/env node
+import {readFile} from 'node:fs/promises';
+import process from 'node:process';
 import {lint} from '../lib/quick-lint.js';
 
 const fileName = process.argv[2];
@@ -8,7 +10,7 @@ if (!fileName) {
     process.exit(0);
 }
 
-const source = await readFile(fileName, "utf-8");
+const source = await readFile(fileName, 'utf-8');
 const places = await lint(source);
 
 console.log(places);
